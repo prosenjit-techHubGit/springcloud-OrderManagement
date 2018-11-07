a733174da237409d925a9f125ef44ff3569b3fc3
Prosenjit.Das <cosypro@gmail.com>
2018-11-07 12:57:15 +0530

    First commit

---
+++ customer-service/src/main/java/io/das/account/customer/controller/CustomerController.java
@@ -0,0 +1,17 @@
+package io.das.account.customer.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/customer")
+public class CustomerController {
+
+	@GetMapping
+	public String getCustomer() {
+
+		return "Hello from Customer service";
+	}
+
+}
